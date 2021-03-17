310eff5b8f9286c40cc24e27d3407565a6b9206f
nikolabibercic <nikolabibercic@gmail.com>
2021-03-18 00:45:46 +0100

    uradjean jedan deo top bara i headera

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://kit.fontawesome.com/53eca7a38b.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+<body>
+    <!-- top bar -->
+    <section class="topBar">
+        <div class="container">
+            <a href="mailto:nikolabibercic@gmail.com"><i class="fas fa-envelope-open-text">&nbsp;proba@gmail.com</i></a>
+            <a href="tel:+38163666555"><i class="fas fa-phone-alt">&nbsp;+381 63 666 555</i></a>
+        </div>
+    </section>
+
+    <!-- header -->
+    <header>
+        <!-- nav bar -->
+        <nav class="container">
+            <div class="logo">
+                <a href="index.php">Logo</a>
+            </div>
+            <ul>
+                <li><a href="">Početna</a></li>
+                <li><a href="">O nama</a></li>
+                <li><a href="">Oblast rada</a></li>
+                <li><a href="">Kontakt</a></li>
+            </ul>
+        </nav>
+        <hgroup>
+            <h1>Advokat Novi Sad</h1>
+            <p>Višegodišnje iskustvo u različitim oblasima prava</p>
+        </hgroup>
+    </header>
+
+</body>
+</html>
