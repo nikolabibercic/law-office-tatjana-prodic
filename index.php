d1a79912ab0ccc7e09aa1aca5a2e0c9647f0b46f
nikolabibercic <nikolabibercic@gmail.com>
2021-03-18 19:21:17 +0100

    na index stranici uradjen footer, mapa, kontatkt forma i sekcija o nama

--- index.php
+++ index.php
@@ -25,10 +25,10 @@
                 <a href="index.php">Logo</a>
             </div>
             <ul>
-                <li><a href="">Početna</a></li>
-                <li><a href="">O nama</a></li>
-                <li><a href="">Oblast rada</a></li>
-                <li><a href="">Kontakt</a></li>
+                <li><a href="index.php">Početna</a></li>
+                <li><a href="about-us.php">O nama</a></li>
+                <li><a href="fields-of-work.php">Oblast rada</a></li>
+                <li><a href="contact.php">Kontakt</a></li>
             </ul>
         </nav>
         <hgroup>
@@ -36,6 +36,39 @@
             <p>Višegodišnje iskustvo u različitim oblasima prava</p>
         </hgroup>
     </header>
+
+    <!-- about us -->
+    <section class="aboutUs container">
+        <article>
+            <h2>O nama</h2>
+            <hr>
+            <p><img src="img/about-us.jpg" alt="">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, incidunt animi voluptas modi aut excepturi exercitationem odio cum natus voluptatem, impedit ut tempore sit atque dolores consectetur. Excepturi quae voluptatem aliquam incidunt expedita similique vel, ullam reprehenderit blanditiis, odio architecto quisquam saepe sequi, aspernatur sit necessitatibus temporibus id mollitia dolorem omnis in debitis sint quos! Soluta ea illum commodi tempora minus iste vitae voluptas corporis modi architecto libero, quod, impedit sunt, a nostrum iusto asperiores reprehenderit atque hic? Deleniti, tempore et cumque impedit voluptatum a vero fuga voluptatem quisquam corporis. Distinctio molestias consequatur libero consectetur nulla cupiditate eius qui fuga incidunt iusto eveniet laboriosam voluptate asperiores nostrum maiores, in placeat rerum quis facere, labore similique voluptates odit veniam! Nemo alias excepturi nesciunt beatae vitae quae et recusandae inventore minus aliquam, rem odio neque iste consequatur maxime totam delectus eaque mollitia sapiente eveniet corrupti! Voluptatibus, vitae! Sunt, quasi recusandae necessitatibus consequatur optio iure modi, repellat, veniam voluptatibus delectus nobis. Id quaerat labore eius porro! Consectetur neque quas, cumque ab eos sint perspiciatis voluptas aut sit harum atque soluta sapiente asperiores facere enim obcaecati, saepe corrupti veniam optio necessitatibus amet iusto doloremque nisi? Corrupti, dignissimos fugit id aliquid totam eius nisi qui amet officiis, assumenda alias velit nostrum dolor, officia minima eum quod quidem sed natus voluptas ducimus maiores molestiae impedit? Reiciendis distinctio enim non in, molestiae consequuntur itaque nam provident, magni deserunt vel fugiat dolorem accusantium laborum nesciunt incidunt fugit laboriosam eligendi tempora iste, inventore sunt sed cupiditate deleniti? Debitis pariatur aliquid asperiores, voluptatibus dicta, ipsa neque dolore tempora expedita eos laboriosam. Facere optio accusamus repellendus aperiam praesentium perferendis saepe excepturi. Ipsa veritatis eveniet nam aliquid, repellendus ratione hic pariatur similique nisi nulla dicta nemo autem non quas quod quae eum saepe quibusdam fuga accusamus minima a libero soluta corrupti. Voluptatum?</p>
+        </article>    
+    </section>
+
+    <!-- contact form -->
+    <section class="contactForm container">
+            <h2>Kontakt</h2>
+            <hr>
+            <form action="">
+                <div class="input">
+                    <input type="email" placeholder="Email">
+                    <input type="text" placeholder="Ime i prezime">
+                </div>
+                <textarea name="Poruka" id="" cols="30" rows="10" placeholder="Poruka"></textarea>
+                <button>Pošalji</button>
+            </form>
+    </section>
+
+    <!-- map -->
+    <section class="map">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2808.9916469530385!2d19.83983091542249!3d45.247961679099!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x475b1014c9f306d5%3A0x22da33789f2bd85!2sDimitrija%20Tucovi%C4%87a%202a%2C%20Novi%20Sad!5e0!3m2!1sen!2srs!4v1616084466269!5m2!1sen!2srs" width="100%" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe>
+    </section>
+
+    <!-- footer -->
+    <footer>
+        <p>&copy; Advokatska kancelarija Tatjane Prodić</p>
+    </footer>
 
 </body>
 </html>
